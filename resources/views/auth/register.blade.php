3699997d326e8d773e822895d6f66117f8ec7ef3
jbertrius <ezin.jean.bertrand@gmail.com>
2016-09-22 19:52:24 +0100

    Update #6

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -34,7 +34,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="">Pideo</a>
+                <a class="navbar-brand" href="/">Pideo</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="top-navbar-1">
